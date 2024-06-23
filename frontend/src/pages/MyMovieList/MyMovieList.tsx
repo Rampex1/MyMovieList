ef63f719bf9e80368b8941919bfa396c811bada2
David Zhou <david.zhou.0110@gmail.com>
2024-06-23 16:29:27 -0400

    DEV: Created folders for frontend

---
+++ frontend/src/pages/MyMovieList/MyMovieList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyMovieList: React.FC = () => {
+    return (
+        <div>
+            <h1>MyMovieList</h1>
+        </div>
+    );
+  };
+  
+  export default MyMovieList;
